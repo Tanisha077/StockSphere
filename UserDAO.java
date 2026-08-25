e13868835777473837247d61b9dacd735d16cb5c
Tanisha Soni <ts1559954@gmail.com>
2026-08-25 18:08:51 +0530

    Create UserDAO.java

---
+++ UserDAO.java
@@ -0,0 +1,45 @@
+package com.stocksphere.dao;
+
+import com.stocksphere.database.DBConnection;
+import com.stocksphere.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+    public User login(String username, String password) {
+
+        User user = null;
+
+        try {
+
+            Connection con = DBConnection.getConnection();
+
+            String sql = "SELECT * FROM users WHERE username=? AND password=?";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                user = new User();
+
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+}
